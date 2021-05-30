aa0fc1953c14cb6ab300885d11539553a0e81ebc
Bartosz Michalski <bmichalski86@gmail.com>
2021-05-30 23:28:10 +0200

    Added initial project files

---
+++ ChipEmu/Emulator.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Chip
+{
+	public class Emulator
+	{
+	}
+}
---
+++ ChipTests/EmulatorTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChipTests
+{
+	[TestClass]
+	public class EmulatorTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
