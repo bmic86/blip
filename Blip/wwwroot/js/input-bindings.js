5ee921c5a83bb11ef9090f82506b0136bf8e6a75
Bartosz Michalski <bmichalski86@gmail.com>
2021-12-20 23:44:57 +0100

    Added initial implementation of input handling.

---
+++ Blip/wwwroot/js/input-bindings.js
@@ -0,0 +1,27 @@
+﻿let _dotNetHandler = null;
+
+let onKeyUp = (e) => {
+  _dotNetHandler.invokeMethod('OnDocumentKeyUp', e.code)
+}
+
+let onKeyDown = (e) => {
+  _dotNetHandler.invokeMethod('OnDocumentKeyDown', e.code)
+}
+
+export function registerInputHandlers(dotNetHandler) {
+  if (_dotNetHandler == null) {
+    document.addEventListener('keyup', onKeyUp)
+    document.addEventListener('keydown', onKeyDown)
+
+    _dotNetHandler = dotNetHandler
+  }
+}
+
+export function unregisterInputHandlers() {
+  if (_dotNetHandler != null) {
+    document.removeEventListener('keyup', onKeyUp)
+    document.removeEventListener('keydown', onKeyDown)
+
+    _dotNetHandler = null
+  }
+}
