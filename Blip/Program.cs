14ab148cc10667480cb3066ebf90156e9e9b9380
Bartosz Michalski <bmic86@users.noreply.github.com>
2023-03-04 21:28:37 +0100

    Project dependencies update
    * Updated project to .NET 7.
    * Updated NuGet packages.

--- Blip/Program.cs
+++ Blip/Program.cs
@@ -15,7 +15,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services
-    .AddBlazorise(options => { options.ChangeTextOnKeyPress = true; })
+    .AddBlazorise()
     .AddBootstrap5Providers()
     .AddFontAwesomeIcons();
 
